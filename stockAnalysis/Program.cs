92b1d36eefbad268486acf6d8f4499a0bd16b1fc
Ryan Huse <rmhks95@ksu.edu>
2018-11-02 17:53:22 -0500

    Completed the criteria sort

--- stockAnalysis/Program.cs
+++ stockAnalysis/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,14 +18,107 @@ namespace stockAnalysis
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            parseCriteria();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            
         }
 
 
         static void parseCriteria()
         {
+            string line;
+            int colCounter = 0;
+            int CriteriaCount = -1;
+            string agKey = "";
+            string agSum = "";
+            string [] preProcess = new string[10];
+            string[] preAg = new string[10];
+            string[,] preValues = new string[10, 15];
+            string[] postProcess = new string[10];
+            string[] postAg = new string[10];
+            string[,] postValues = new string[10, 15];
+            Criteria criteria = new Criteria();
+            List<Criteria> list = new List<Criteria>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"U:\\stockAnalysis\\stockAnalysis\\criteria sets.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (!line.StartsWith("--"))
+                {
+                    if (line.StartsWith("!"))
+                    {
+                        criteria = new Criteria();
+                        criteria.Name = line.Substring(1);
+                        CriteriaCount = -1;
+                        agKey = "";
+                        preAg = new string[10];
+                        postAg = new string[10];
+                    }
+                    else if (line.StartsWith("@"))
+                    {
+                        CriteriaCount++;
+                        preAg[CriteriaCount] = line.Substring(1);
+                        colCounter = 0;
+                    }
+                    else if (line.StartsWith("^"))
+                    {
+                        preProcess[CriteriaCount] = line.Substring(1);
+                    }
+                    else if (line.StartsWith("#"))
+                    {
+                        if (Regex.IsMatch(line, @"[A-Za-z]"))
+                        {
+                            preValues[CriteriaCount, colCounter] = line.Substring(1);
+                            colCounter++;
+                        }
+                        if (Regex.IsMatch(line, @"\d"))
+                        {
+                            postValues[CriteriaCount, colCounter] = line.Substring(1);
+                            colCounter++;
+                        }
+                    }
+                    else if (line.StartsWith("*"))
+                    {
+                        agKey += line.Substring(1);
+                        agKey += ",";
+                    }
+                    else if (line.StartsWith("+"))
+                    {
+                        agSum += line.Substring(1);
+                        agSum += ",";
+                        CriteriaCount = -1;
+                    }
+                    else if (line.StartsWith("$"))
+                    {
+                        CriteriaCount++;
+                        postAg[CriteriaCount] = line.Substring(1);
+                        colCounter = 0;
+                    }
+                    else if (line.StartsWith("&"))
+                    {
+                        postProcess[CriteriaCount] = line.Substring(1);
+                    }
+
+                    if (string.IsNullOrEmpty(line) && file.ReadLine().StartsWith("--") || file.EndOfStream)
+                    {
+                        if (criteria.Name != null)
+                        {
+                            criteria.PreAgCol = preAg;
+                            criteria.PreProcess = preProcess;
+                            criteria.PreValues = preValues;
+                            criteria.agKey = agKey.Substring(0, agKey.Length - 1);
+                            criteria.agSum = agSum.Substring(0, agSum.Length - 1);
+                            criteria.PostAgCol = postAg;
+                            criteria.PostProcess = postProcess;
+                            criteria.PostValues = postValues;
+                            list.Add(criteria);
+                        }
+                    }
+                }
+
+            }
 
         }
     }
---
+++ stockAnalysis/criteria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stockAnalysis
+{
+    public class Criteria
+    {
+        public string Name { get; set; }
+        public string[] PreAgCol { get; set; }
+        public string[] PreProcess { get; set; }
+        public string[,] PreValues { get; set; }
+        public string agKey { get; set; }
+        public string agSum { get; set; }
+        public string[] PostAgCol { get; set;}
+        public string[] PostProcess { get; set; }
+        public string[,] PostValues { get; set; }
+        //public Criteria(string name, string[] preAgCol, string preProcess, string[,] prevalues)
+        //{
+        //    Name = name;
+        //    PreAgCol = preAgCol;
+        //    PreProcess = preProcess;
+        //    PreValues = PreValues;
+        //}
+        
+            //Other properties, methods, events...
+        
+    }
+}
